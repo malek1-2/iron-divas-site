cb906656e4f9aa4ee959177ed8c11c00210b85b6
Your Name <you@example.com>
2026-06-12 00:33:22 +0100

    add orders api endpoint

--- api/orders.ts
+++ api/orders.ts
@@ -1,4 +1,8 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { createServer } from "../server/index.js";
 
 const app = createServer();
-export default app;
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  return app(req, res);
+}
